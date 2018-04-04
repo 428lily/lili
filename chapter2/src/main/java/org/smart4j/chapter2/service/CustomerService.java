4315cae636056031faf7c55e5c91d59385003b3c
jyl吉燕莉 <yanliji@DST67211>
2018-04-04 16:29:10 +0800

    jdbc 4/4

--- chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -4,103 +4,76 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.smart4j.chapter2.helper.DatabaseHelper;
 import org.smart4j.chapter2.model.Customer;
-import org.smart4j.chapter2.util.PropsUtil;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
-
 
 /**
  * 提供客户数据服务
  */
 public class CustomerService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
-    /**
-     * 获取客户列表
-     *
-     * @return
-     */
-    public List<Customer> getCustomerList() {
-        Connection conn = null;
-        List<Customer> customerList = new ArrayList<Customer>();
-        try {
-            String sql = "select * from customer";
-            conn = DatabaseHelper.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()){
-                Customer customer = new Customer();
-                customer.setId(rs.getLong("id"));
-                customer.setName(rs.getString("name"));
-                customer.setContact(rs.getString("contact"));
-                customer.setTelephone(rs.getString("telephone"));
-                customer.setEmail(rs.getString("email"));
-                customer.setRemark(rs.getString("remark"));
-                customerList.add(customer);
-            }
-        } catch (SQLException e) {
-            LOGGER.error("execute sql failure",e);
-        }finally {
-            DatabaseHelper.closeConnection(conn);
-        }
-        return customerList;
-    }
+  /**
+   * 获取客户列表
+   *
+   * @return
+   */
+  public List<Customer> getCustomerList() {
+    String sql = "select * from customer";
+    return DatabaseHelper.queryEntityList(Customer.class, sql);
+  }
 
-    /**
-     * 获取客户
-     *
-     * @return
-     */
-    public Customer getCustomer(long id) {
-        Connection conn = null;
-        try {
-            String sql = "select * from customer where id = "+id;
-            PreparedStatement stmt = conn.prepareStatement(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+  /**
+   * 获取客户
+   *
+   * @return
+   */
+  public Customer getCustomer(long id) {
+    Connection conn = null;
+    try {
+      String sql = "select * from customer where id = " + id;
+      PreparedStatement stmt = conn.prepareStatement(sql);
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
+    return null;
+  }
 
-    /**
-     * 创建客户
-     *
-     * @param fieldMap
-     * @return
-     */
-    public boolean createCustomer(Map<String, Object> fieldMap) {
-        // TODO
-        return false;
-    }
+  /**
+   * 创建客户
+   *
+   * @param fieldMap
+   * @return
+   */
+  public boolean createCustomer(Map<String, Object> fieldMap) {
+    // TODO
+    return false;
+  }
 
-    /**
-     * 更新客户
-     *
-     * @param id
-     * @param fieldMap
-     * @return
-     */
-    public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
-        // TODO
-        return false;
-    }
+  /**
+   * 更新客户
+   *
+   * @param id
+   * @param fieldMap
+   * @return
+   */
+  public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
+    // TODO
+    return false;
+  }
 
-    /**
-     * 删除客户
-     *
-     * @param id
-     * @return
-     */
-    public boolean deleteCustomer(long id) {
-        // TODO
-        return false;
-    }
+  /**
+   * 删除客户
+   *
+   * @param id
+   * @return
+   */
+  public boolean deleteCustomer(long id) {
+    // TODO
+    return false;
+  }
 }
